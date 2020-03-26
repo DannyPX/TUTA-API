1d4fb274fe5c630fcb04df6dcf32644581900a2c
DannyPX <Damnoga@hotmail.com>
2020-03-26 14:41:14 +0100

    port to heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
-const port = 5000;
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const PORT = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
