d78b0f5727832d173b1fba96fe02d8a913042f8d
DannyPX <Damnoga@hotmail.com>
2020-03-26 14:37:06 +0100

    port change

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const port = 3000;
+const port = 5000;
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
